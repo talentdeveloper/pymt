ce0b798fafecc6a46fb847ea8610e592dc9bb9ff
Anchit <solverhp@yahoo.com>
2018-03-02 22:19:28 +0530

    Signed-off-by: Anchit <solverhp@yahoo.com>

--- ReactNative/PytmNative/Pytm/index.js
+++ ReactNative/PytmNative/Pytm/index.js
@@ -3,9 +3,14 @@ import { StackNavigator, } from 'react-navigation';
 import App from './App';
 import LoginScreen from './Screens/LoginScreen';
 import PinScreen from './Screens/PinScreen';
+import OpenAmountScreen from './Screens/OpenAmountScreen';
+import CashdropAmmountScreen from './Screens/CashdropAmountScreen';
+import categoryScreen from './Screens/CategroyScreen';
 
 const navigation = StackNavigator({
-
+    categoryScreen: { screen: categoryScreen},
+    CashdropAmmountScreen: { screen: CashdropAmmountScreen},
+    OpenAmountScreen: { screen: OpenAmountScreen},
     PinScreen: { screen: PinScreen},
     LoginScreen: { screen: LoginScreen },
 
